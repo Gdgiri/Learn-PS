fec2b6709fd42d3446779e5c69fbb0924b11bb26
giridharan <giridharan831@gmail.com>
2024-11-13 19:54:15 +0530

    commit the friendly number or not

--- Task/friendlyOrNot.js
+++ Task/friendlyOrNot.js
@@ -11,17 +11,23 @@ function properDivisor(n) {
 
   return { Divisor, sum };
 }
-let num1 = 6;
-let num2 = 28;
 
-let sum1 = properDivisor(num1);
-let sum2 = properDivisor(num2);
-
-let divSum1 = sum1.sum / num1;
-let divSum2 = sum2.sum / num2;
-
-if (divSum1 === divSum2) {
-  console.log(`${num1} and ${num2} are friendly number`);
-} else {
-  console.log(`${num1} and ${num2} are not friendly number`);
+function isFriend(num1,num2){
+    let sum1 = properDivisor(num1);
+    let sum2 = properDivisor(num2);
+    
+    let divSum1 = sum1.sum / num1;
+    let divSum2 = sum2.sum / num2;
+    
+    if (divSum1 === divSum2) {
+      console.log(`${num1} and ${num2} are friendly number`);
+    } else {
+      console.log(`${num1} and ${num2} are not friendly number`);
+    }
 }
+
+isFriend(6,28)
+isFriend(10,20)
+isFriend(140,30)
+
+
