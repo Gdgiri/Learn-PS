ed91819b6f5892d5649cd64084880bd8808e9e61
giridharan <giridharan831@gmail.com>
2024-11-13 19:32:40 +0530

    commit the message it's just practising

--- Day-1/properDivisor.js
+++ Day-1/properDivisor.js
@@ -1,17 +1,24 @@
+// this code is just practising the amicable or not that function is used to find the proper divisor or not without sum
 function properDivisor(n) {
-  let Divisors = [];
-  
-
-  for (let i = 1; i < n - 1; i++) {
+  let Divisor = [];
+  let sum = 0;
+  for (let i = 1; i <= n / 2; i++) {
     if (n % i === 0) {
-      Divisors.push(i);
-      
+      Divisor.push(i);
+      sum += i;
     }
   }
-  return Divisors;
-  
+  return { Divisor, sum };
 }
 
-const number = 220;
-console.log(properDivisor(number));
+let num1 = 220;
+let num2 = 285;
 
+const sum1 = properDivisor(num1);
+const sum2 = properDivisor(num2);
+
+if (num1 === sum2.sum && num2 === sum1.sum) {
+  console.log("amicable number");
+} else {
+  console.log("not amicable ");
+}
