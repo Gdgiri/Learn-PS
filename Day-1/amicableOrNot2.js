de068736b03bcca01fd13735d67cc86aae0c97b6
giridharan <giridharan831@gmail.com>
2024-11-13 19:18:10 +0530

    update the code what i insert new

--- Day-1/amicableOrNot2.js
+++ Day-1/amicableOrNot2.js
@@ -2,26 +2,23 @@ const num1 = 220;
 const num2 = 284;
 
 function properDivisor(n) {
-  const Divisor = [];
+  const divisors = [];
   let sum = 0;
   for (let i = 1; i <= n / 2; i++) {
     if (n % i === 0) {
-      Divisor.push(i);
+      divisors.push(i);
       sum += i;
     }
   }
-  return { Divisor, sum };
+  return { divisors, sum };
 }
 
 const sNum1 = properDivisor(num1);
 const sNum2 = properDivisor(num2);
-console.log(sNum1.Divisor);
-console.log(sNum1.sum);
-console.log(sNum2.Divisor);
-console.log(sNum2.sum);
 
+// Check if the numbers are amicable
 if (sNum1.sum === num2 && sNum2.sum === num1) {
-  console.log(`${num1} and ${num2} is a amicable number`);
+  console.log(`${num1} and ${num2} are amicable numbers`);
 } else {
-  console.log(`${num1} and ${num2} is not a amicable number`);
+  console.log(`${num1} and ${num2} are not amicable numbers`);
 }
